16a109622c8d293702b2b1cb8b8b511c9e8cf3b3
Gabriel Roch <gabriel.roch@heig-vd.ch>
2019-11-26 13:16:57 +0100

    Add cron.py

---
+++ cron.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    Copyright 2019 Gabriel Roch
+
+    This file is part of heig-bot.
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    heig-bot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with heig-bot. If not, see <https://www.gnu.org/licenses/>.
+"""
+import telegram.ext
+import sys
+import subprocess
+import json
+import os
+from init import config
+from user import User
+
+
+
+for i in os.scandir(config["database_directory"]):
+    id = i.name[:-5]
+    user = User(id)
+    user.gaps().check_gaps_notes(id, auto=True)
+
+#for id in config["logs_userid"]:
+    #updater.bot.send_message(chat_id=id, text="Bonjour")
+
